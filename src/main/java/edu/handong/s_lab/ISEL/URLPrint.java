d3a57d918ab88b29bcd20bd066072d6e760a3c4b
Yang sujin <21500394@handong.edu>
2018-04-25 23:56:22 +0900

    make getURL method

--- src/main/java/edu/handong/s_lab/ISEL/URLPrint.java
+++ src/main/java/edu/handong/s_lab/ISEL/URLPrint.java
@@ -1,13 +1,25 @@
 package edu.handong.s_lab.ISEL;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class URLPrint {
-	private URL url;
+	private static URL url;
 
-	public static void main(String[] args) {
+	private void getURL() {
 		URLData u = new URLData();
 		u.address = "https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc";
+		try {
+			url = new URL(u.address);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		URLPrint p = new URLPrint();
+		p.getURL();
 
 	}
 
