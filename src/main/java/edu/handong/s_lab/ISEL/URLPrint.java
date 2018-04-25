4259e3915f2301f91c879bd516995c6c2948b86e
Yang sujin <21500394@handong.edu>
2018-04-25 23:50:34 +0900

    save address

---
+++ src/main/java/edu/handong/s_lab/ISEL/URLPrint.java
@@ -0,0 +1,14 @@
+package edu.handong.s_lab.ISEL;
+
+import java.net.URL;
+
+public class URLPrint {
+	private URL url;
+
+	public static void main(String[] args) {
+		URLData u = new URLData();
+		u.address = "https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc";
+
+	}
+
+}
