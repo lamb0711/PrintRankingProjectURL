26ee39e461496ecc911368739b4e697e4e10b52b
Yang sujin <21500394@handong.edu>
2018-04-26 00:18:59 +0900

    add HttpURLConnection variable

--- src/main/java/edu/handong/s_lab/ISEL/URLPrint.java
+++ src/main/java/edu/handong/s_lab/ISEL/URLPrint.java
@@ -1,5 +1,6 @@
 package edu.handong.s_lab.ISEL;
 
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -7,12 +8,16 @@ import java.util.ArrayList;
 public class URLPrint {
 	private static URL url;
 	public ArrayList<String> eachAddress;
+	public HttpURLConnection code;
 
 	private void getURL() {
 		String address = "https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc";
 		
 		try {
 			url = new URL(address);
+			eachAddress = new ArrayList<String>();
+			
+			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
