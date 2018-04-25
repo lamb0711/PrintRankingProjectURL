cbfe6e526c9924988fcda7346a7fa5b075149921
Yang sujin <21500394@handong.edu>
2018-04-26 03:08:28 +0900

    add code for read URL

--- src/main/java/edu/handong/s_lab/ISEL/URLPrint.java
+++ src/main/java/edu/handong/s_lab/ISEL/URLPrint.java
@@ -1,23 +1,34 @@
 package edu.handong.s_lab.ISEL;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class URLPrint {
-	private static URL url;
+	private static URL url;//URL address
 	public ArrayList<String> eachAddress;
-	public HttpURLConnection code;
+	public HttpURLConnection code;//connection with wep
+	public BufferedReader br;//read URL code line
 
-	private void getURL() {
+	private void getURL() throws IOException {
 		String address = "https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc";
 		
 		try {
-			url = new URL(address);
+			url = new URL(address);//make URL object
 			eachAddress = new ArrayList<String>();
+			code = (HttpURLConnection)url.openConnection();//connection with this address
+			//code.setRequestMethod("GET");
 			
+			br = new BufferedReader(new InputStreamReader(code.getInputStream()));
 			
+			String line;
+			while((line = br.readLine()) != null) {
+				
+			}
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
