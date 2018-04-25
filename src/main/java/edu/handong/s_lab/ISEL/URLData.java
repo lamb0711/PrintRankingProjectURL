de1761ab8c2d50051e120c34c7d35af5c83a03ac
Yang sujin <21500394@handong.edu>
2018-04-25 23:33:36 +0900

    make class that save URL data

---
+++ src/main/java/edu/handong/s_lab/ISEL/URLData.java
@@ -0,0 +1,25 @@
+package edu.handong.s_lab.ISEL;
+
+import java.net.URL;
+
+public class URLData {
+	public String address;
+	public URL url;
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public URL getUrl() {
+		return url;
+	}
+	
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+
+}
